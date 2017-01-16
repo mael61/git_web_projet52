9d6d0b5019f9f5c51a8f72b088b1b4456e74119d
maillard <mm6195@hotmail.fr>
2017-01-16 17:22:03 +0100

    Le meme delire avec les photo pour tester changer la date de windows

--- semaine.php
+++ semaine.php
@@ -1,11 +1,22 @@
-<?php 
-		if(($_SESSION==null)){
-	echo '
+<?php
+$host="localhost";    
+$username="mael61";
+$password="github" ;
+ 
+$db_name="projet_52";  
+$con = new PDO('mysql:host='.$host.';dbname='.$db_name, $username, $password);
+$Requete = $con->prepare('select fichier from image where theme = (SELECT nom FROM `theme` WHERE `date`=CURDATE()-7)');
+			$Requete->execute();
+			$troisPhoto = $Requete->fetchAll(PDO::FETCH_COLUMN, 0);
+			
+?>			
+
+	
 <div id="fh5co-work-section" class="fh5co-light-grey-section">
 		<div class="row">
 				<div class="col-md-4 animate-box">
 					<a href="#" class="item-grid text-center">
-						<div class="image" style="background-image: url(images/project-1.jpg)"></div>
+						<div class="image" style="background-image: url(images/<?php echo($troisPhoto[0]);?>)"></div>
 						<div class="v-align">
 							<div class="v-align-middle">
 								<h4 class="title">Évolution</h4>
@@ -16,7 +27,7 @@
 				</div>
 				<div class="col-md-4 animate-box">
 					<a href="#" class="item-grid text-center">
-						<div class="image" style="background-image: url(images/project-2.jpg)"></div>
+						<div class="image" style="background-image: url(images/<?php echo($troisPhoto[1]);?>)"></div>
 						<div class="v-align">
 							<div class="v-align-middle">
 								<h4 class="title">Évolution</h4>
@@ -27,7 +38,7 @@
 				</div>
 				<div class="col-md-4 animate-box">
 					<a href="#" class="item-grid text-center">
-						<div class="image" style="background-image: url(images/project-3.jpg)"></div>
+						<div class="image" style="background-image: url(images/<?php echo($troisPhoto[2]);?>)"></div>
 						<div class="v-align">
 							<div class="v-align-middle">
 								<h4 class="title">Évolution</h4>
@@ -39,5 +50,5 @@
 			</div>
 			</div>
 	</div>
-		';}
-	?>
+		
+	
