9da8e92b00168868f2c8cf9fba1e87cb1bbe27a6
maillard <mm6195@hotmail.fr>
2017-01-17 12:02:50 +0100

    meme commit que le dernier il manquait juste le fichier semaine

--- semaine.php
+++ semaine.php
@@ -5,11 +5,38 @@ $password="github" ;
  
 $db_name="projet_52";  
 $con = new PDO('mysql:host='.$host.';dbname='.$db_name, $username, $password);
-$Requete = $con->prepare('select fichier from image where theme = (SELECT nom FROM `theme` WHERE `date`=CURDATE()-7)');
+$Requete = $con->prepare('select fichier ,auteur from image where theme = (SELECT nom FROM `theme` WHERE `date`<=CURDATE()-7 and `date`>=CURDATE()-13 )');
 			$Requete->execute();
 			$troisPhoto = $Requete->fetchAll(PDO::FETCH_COLUMN, 0);
+			print_r($troisPhoto);
+			$Requete->execute();
+			$troisPhotoNom = $Requete->fetchAll(PDO::FETCH_COLUMN, 1);
+			print_r($troisPhotoNom);
+$Requete2 = $con->prepare('SELECT nom FROM `theme` WHERE `date`<=CURDATE()-7 and `date`>=CURDATE()-13');
+			$Requete2->execute();
+			$nomThemePage = $Requete2->fetchColumn();
 			
-?>			
+?>		
+<div id="fh5co-project">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Un thème par semaine</h2>
+				<?php 
+				
+				 if(($_SESSION)){
+					echo 'Bonjour  '.$_SESSION['pseudo'].'';
+				 
+					echo '<a href="deconnexion.php">Déconnexion</a>';
+					echo'<form action="upload.php" method="post" enctype="multipart/form-data">
+						Selectionne une image:
+					<input type="file" name="fileToUpload" id="fileToUpload">
+					<input type="submit" value="Upload Image" name="submit">
+						</form>';
+				 }?>
+				</div>
+			</div>
+		</div>	
 
 	
 <div id="fh5co-work-section" class="fh5co-light-grey-section">
@@ -19,8 +46,8 @@ $Requete = $con->prepare('select fichier from image where theme = (SELECT nom FR
 						<div class="image" style="background-image: url(images/<?php echo($troisPhoto[0]);?>)"></div>
 						<div class="v-align">
 							<div class="v-align-middle">
-								<h4 class="title">Évolution</h4>
-								<h5 class="category">Maël Maillard</h5>
+								<h4 class="title"> thème: <?php echo($nomThemePage) ;?></h4>
+								<h5 class="category"><?php echo($troisPhotoNom[0]);?></h5>
 							</div>
 						</div>
 					</a>
@@ -30,8 +57,8 @@ $Requete = $con->prepare('select fichier from image where theme = (SELECT nom FR
 						<div class="image" style="background-image: url(images/<?php echo($troisPhoto[1]);?>)"></div>
 						<div class="v-align">
 							<div class="v-align-middle">
-								<h4 class="title">Évolution</h4>
-								<h5 class="category">Jimmy Sobandith</h5>
+								<h4 class="title">thème: <?php echo($nomThemePage) ;?></h4>
+								<h5 class="category"><?php echo($troisPhotoNom[1]);?></h5>
 							</div>
 						</div>
 					</a>
@@ -41,14 +68,14 @@ $Requete = $con->prepare('select fichier from image where theme = (SELECT nom FR
 						<div class="image" style="background-image: url(images/<?php echo($troisPhoto[2]);?>)"></div>
 						<div class="v-align">
 							<div class="v-align-middle">
-								<h4 class="title">Évolution</h4>
-								<h5 class="category">Alix Nouzillat</h5>
+								<h4 class="title">thème: <?php echo($nomThemePage) ;?></h4>
+								<h5 class="category"><?php echo($troisPhotoNom[2]);?></h5>
 							</div>
 						</div>
 					</a>
 				</div>
 			</div>
 			</div>
-	</div>
+</div>
 		
 	
